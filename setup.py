5d8a7c221780f7db4f272d1486f2fc13278e7bd2
Michael Droettboom <mdboom@gmail.com>
2019-03-14 08:42:02 -0400

    Bump version: 0.17.0 → 0.17.1

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mozilla/glean_parser',
-    version='0.17.0',
+    version='0.17.1',
     zip_safe=False,
 )
