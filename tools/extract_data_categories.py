e1b35f9003a252a7b18c4fade66b952ab7d81c5c
Michael Droettboom <mdboom@gmail.com>
2020-07-17 09:42:16 -0400

    Add license header

--- tools/extract_data_categories.py
+++ tools/extract_data_categories.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """
 Usage:
    python extract_data_categories.py metrics.yaml
