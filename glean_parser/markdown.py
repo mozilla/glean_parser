7b8bcc74147f187d5973125119af6edea0b5f20c
Michael Droettboom <mdboom@gmail.com>
2020-07-21 18:43:01 -0400

    Bug 1608838: Include data collection categories as metadata (#199)
    * Bug 1608838: Include data collection categories as metadata
    
    * Add license header
    
    * Add HISTORY entry

--- glean_parser/markdown.py
+++ glean_parser/markdown.py
@@ -132,6 +132,13 @@ def ping_include_client_id(
         return False
 
 
+def data_category_numbers(data_category: Optional[List[metrics.DataCategory]]) -> str:
+    if data_category is None:
+        return "unknown"
+    else:
+        return ", ".join(str(x.value) for x in data_category)
+
+
 def output_markdown(
     objs: metrics.ObjectTree, output_dir: Path, options: Dict[str, Any] = {}
 ) -> None:
@@ -209,6 +216,7 @@ def output_markdown(
                 "ping_include_client_id",
                 lambda x: ping_include_client_id(x, custom_pings_cache),
             ),
+            ("data_category_numbers", data_category_numbers),
         ),
     )
 
