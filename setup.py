4e9da3be1aecbcf291b5049a9d92b00097a0e21c
Michael Droettboom <mdboom@gmail.com>
2020-09-14 16:05:21 -0400

    Add Python 3.9 to CI (#226)
    * Add Python 3.9 to CI
    
    Also (while there) upgrades earlier Python versions to the most recent patch
    releases.
    
    * Remove hack to not run black on Python 3.5

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="Parser tools for Mozilla's Glean telemetry",
     entry_points={
