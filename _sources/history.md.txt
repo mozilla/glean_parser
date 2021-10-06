e51b1febead313660c3e4bd1076443fa4c932b9e
glean-ci <glean-ci@nowhere.com>
2021-10-06 18:51:36 +0000

    [ci skip] updates

--- _sources/history.md.txt
+++ _sources/history.md.txt
@@ -3,6 +3,7 @@
 ## Unreleased
 
 - Improve the schema validation error messages. They will no longer include `OrderedDict(...)` on Python 3.7 and later ([bug 1733395](https://bugzilla.mozilla.org/show_bug.cgi?id=1733395))
+- Officially support Python 3.10
 
 ## 4.1.1 (2021-09-28)
 
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1.2.dev6+g86d4dd07',
+    VERSION: '4.1.2.dev8+gfc3cff3a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
