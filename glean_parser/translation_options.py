6e59f17d0cd326cdfdfecf2f9f67791be4292639
perrymcmanis144 <95651456+perrymcmanis144@users.noreply.github.com>
2022-04-20 12:20:32 -0500

    Update glean_parser/translation_options.py
    Co-authored-by: Jan-Erik Rediger <badboy@archlinux.us>

--- glean_parser/translation_options.py
+++ glean_parser/translation_options.py
@@ -9,13 +9,13 @@ These are backend specific and optional, provide as key:value
 Rust: no options.
 
 Swift:
-- namespace: The namespace to generate metrics in
-- glean_namespace: The namespace to import Glean from
-- allow_reserved: When True, this is a Glean-internal build
-- with_buildinfo: If "true" the `GleanBuildInfo` is generated.
+- `namespace`: The namespace to generate metrics in
+- `glean_namespace`: The namespace to import Glean from
+- `allow_reserved`: When True, this is a Glean-internal build
+- `with_buildinfo`: If "true" the `GleanBuildInfo` is generated.
         Otherwise generation of that file is skipped.
         Defaults to "true".
-- build_date: If set to `0` a static unix epoch time will be used.
+- `build_date`: If set to `0` a static unix epoch time will be used.
         If set to a ISO8601 datetime string (e.g. `2022-01-03T17:30:00`)
         it will use that date.
         Other values will throw an error.
