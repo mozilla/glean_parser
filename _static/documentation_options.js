b252c21e203a677cbc2358b8b4137f51a1d94d19
glean-ci <glean-ci@nowhere.com>
2023-05-10 08:48:15 +0000

    [ci skip] updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.2.1.dev2+g63acb9d6',
+    VERSION: '7.2.2.dev1+g6f460cd8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
