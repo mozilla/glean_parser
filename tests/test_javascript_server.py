c8492612a7a0dbbb6511591357dfaecdfc72f77c
Jan-Erik Rediger <jrediger@mozilla.com>
2023-10-10 16:43:13 +0200

    Add an integration-like test for the javascript_server logger

--- tests/test_javascript_server.py
+++ tests/test_javascript_server.py
@@ -4,9 +4,14 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 from pathlib import Path
+import io
+import pytest
+import shutil
+import subprocess
 
 from glean_parser import javascript_server
 from glean_parser import translate
+from glean_parser import validate_ping
 
 
 ROOT = Path(__file__).parent
@@ -67,3 +72,61 @@ def test_generate_ping_factory_method():
     expected_result = "createAccountsEventsEvent"
     result = javascript_server.generate_ping_factory_method(ping)
     assert result == expected_result
+
+
+def run_logger(code_dir, import_file, factory, code):
+    """
+    Run the JavaScript logger with a mocked logger
+    that just prints the ping payload to STDOUT.
+    """
+
+    shutil.copy(ROOT / "test-js" / "package.json", code_dir)
+    subprocess.check_call(["npm", "install"], cwd=code_dir)
+
+    tmpl_code = ""
+    with open(ROOT / "test-js" / "test.js.tmpl", "r") as fp:
+        tmpl_code = fp.read()
+
+    tmpl_code = (
+        tmpl_code.replace("/* IMPORT */", import_file)
+        .replace("/* FACTORY */", factory)
+        .replace("/* CODE */", code)
+    )
+
+    with open(code_dir / "test.js", "w") as fp:
+        fp.write(tmpl_code)
+
+    return subprocess.check_output(["node", "test.js"], cwd=code_dir).decode("utf-8")
+
+
+@pytest.mark.web_dependency
+def test_run_logging(tmpdir):
+    tmpdir = Path(str(tmpdir))
+
+    translate.translate(
+        [
+            ROOT / "data" / "fxa-server-pings.yaml",
+            ROOT / "data" / "fxa-server-metrics.yaml",
+        ],
+        "javascript_server",
+        tmpdir,
+    )
+
+    factory = "createAccountsEventsEvent"
+    code = """
+events.record({ user_agent: "glean-test/1.0", event_name: "testing" });
+    """
+
+    payload = run_logger(tmpdir, "server_events.js", factory, code)
+
+    schema_url = (
+        "https://raw.githubusercontent.com/mozilla-services/"
+        "mozilla-pipeline-schemas/main/"
+        "schemas/glean/glean/glean.1.schema.json"
+    )
+
+    input = io.StringIO(payload)
+    output = io.StringIO()
+    assert (
+        validate_ping.validate_ping(input, output, schema_url=schema_url) == 0
+    ), output.getvalue()
