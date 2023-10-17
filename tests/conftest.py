43cb9bcf61443d6aa369b8f046827ee85ca3923d
Jan-Erik Rediger <jrediger@mozilla.com>
2023-10-17 16:41:09 +0200

    Add specific flag for node tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,23 @@ def pytest_addoption(parser):
         default=False,
         help="Run tests that require a web connection",
     )
+    parser.addoption(
+        "--run-node-tests",
+        action="store_true",
+        default=False,
+        help="Run tests that require node.js",
+    )
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--run-web-tests"):
-        return
-    skip_web = pytest.mark.skip(reason="Need --run-web-tests option to run")
-    for item in items:
-        if "web_dependency" in item.keywords:
-            item.add_marker(skip_web)
+    if not config.getoption("--run-web-tests"):
+        skip_web = pytest.mark.skip(reason="Need --run-web-tests option to run")
+        for item in items:
+            if "web_dependency" in item.keywords:
+                item.add_marker(skip_web)
+
+    if not config.getoption("--run-node-tests"):
+        skip_node = pytest.mark.skip(reason="Need --run-node-tests option to run")
+        for item in items:
+            if "node_dependency" in item.keywords:
+                item.add_marker(skip_node)
