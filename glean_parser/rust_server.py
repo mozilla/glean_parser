5337001c0b8c2418b42ef6ae148a739ed9d52692
Taddes <tkorris@mozilla.com>
2024-10-28 18:09:56 -0400

    utility functions

--- glean_parser/rust_server.py
+++ glean_parser/rust_server.py
@@ -26,6 +26,46 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Any
 
+from . import __version__
+from . import metrics
+from . import util
+
 # Adding a metric here will require updating the `generate_metric_type` function
 # and require adjustments to `metrics` variables the the template.
 SUPPORTED_METRIC_TYPES: list[str] = ["string", "quantity", "event", "datetime", "boolean"]
+
+def generate_ping_type_name(ping_name: str) -> str:
+    return f"{util.Camelize(ping_name)}Ping"
+
+def generate_ping_events_type_name(ping_name: str) -> str:
+    return f"{util.Camelize(ping_name)}PingEvent"
+
+def generate_event_type_name(metric: metrics.Metric) -> str:
+    return f"{util.Camelize(metric.category)}{util.Camelize(metric.name)}Event"
+
+def generate_metric_name(metric: metrics.Metric) -> str:
+    return f"{metric.category}.{metric.name}"
+
+def generate_extra_name(extra: str) -> str:
+    return util.Camelize(extra)
+
+def generate_metric_argument_name(metric: metrics.Metric) -> str:
+    return f"{util.Camelize(metric.category)}{util.Camelize(metric.name)}"
+
+def generate_metric_type(metric_type: str) -> str:
+    match metric_type:
+        case "quantity":
+            return "u64"
+        case "string": 
+            return "String"
+        case "boolean": 
+            return "bool"
+        case "datetime": 
+            return "chrono::DateTime"
+        case _: 
+            print(f"❌ Unable to generate Rust type from metric type: {metric_type}")
+            exit()
+            return "NONE"
+
+def clean_string(s: str) -> str:
+    return s.replace("\n", " ").rstrip()
