7b3f31f2841dd38326cd50d723925890a8d603d1
Taddes <tkorris@mozilla.com>
2024-11-07 08:33:27 -0500

    add conversion logic for snake case, import rust into translate.py

--- glean_parser/rust_server.py
+++ glean_parser/rust_server.py
@@ -32,59 +32,75 @@ from . import util
 
 # Adding a metric here will require updating the `generate_metric_type` function
 # and require adjustments to `metrics` variables the the template.
-SUPPORTED_METRIC_TYPES: list[str] = ["string", "quantity", "event", "datetime", "boolean"]
+SUPPORTED_METRIC_TYPES: list[str] = [
+    "string",
+    "quantity",
+    "event",
+    "datetime",
+    "boolean",
+]
+
 
 def generate_ping_type_name(ping_name: str) -> str:
     """Returns camel-case string of ping name."""
     return f"{util.Camelize(ping_name)}Ping"
 
+
 def generate_ping_events_type_name(ping_name: str) -> str:
     """Returns camel-case event type name."""
     return f"{util.Camelize(ping_name)}PingEvent"
+
 
 def generate_event_type_name(metric: metrics.Metric) -> str:
     """Returns camel-case string of event type."""
     return f"{util.Camelize(metric.category)}{util.Camelize(metric.name)}Event"
 
+
 def generate_metric_name(metric: metrics.Metric) -> str:
     """Returns camel-case string of metric name."""
     return f"{metric.category}.{metric.name}"
+
 
 def generate_extra_name(extra: str) -> str:
     """Returns camel-case string of extra value."""
     return util.Camelize(extra)
 
+
 def generate_snake_case_name(value: str) -> str:
     """Returns a snake-case function or attribute name."""
     return util.camel_or_pascal_to_snake_case(value)
 
+
 def generate_metric_argument_name(metric: metrics.Metric) -> str:
     """Returns camel-case string of metric argument name."""
     return f"{util.Camelize(metric.category)}{util.Camelize(metric.name)}"
+
 
 def generate_metric_type(metric_type: str) -> str:
     """Return string representation of metric type in Rust."""
     match metric_type:
         case "quantity":
             return "u64"
-        case "string": 
+        case "string":
             return "String"
-        case "boolean": 
+        case "boolean":
             return "bool"
-        case "datetime": 
-            return "chrono::DateTime"
-        case _: 
+        case "datetime":
+            return "chrono::DateTime<Utc>"
+        case _:
             print(f"❌ Unable to generate Rust type from metric type: {metric_type}")
             exit()
             return "NONE"
+
 
 def clean_string(s: str) -> str:
     """Strip strings of any newlines and trailing space."""
     return s.replace("\n", " ").rstrip()
 
+
 def output_rust(
-        objs: metrics.ObjectTree, output_dir: Path, options: dict[str, Any] | None
-    ) -> None:
+    objs: metrics.ObjectTree, output_dir: Path, options: dict[str, Any] | None
+) -> None:
     """
     Given a tree of objects, output Rust code to `output_dir`.
 
@@ -109,13 +125,13 @@ def output_rust(
             ("rust_metric_type", generate_metric_type),
             ("clean_string", clean_string),
             ("snake_case", generate_snake_case_name),
-            ),
-        )
-    
+        ),
+    )
+
     # Unique list of event metrics used in any ping.
     event_metrics: list[metrics.Metric] = []
 
-    # Go though all metrics in objs and build a map of 
+    # Go though all metrics in objs and build a map of
     # ping->list of metric categories->list of metrics
     # for easier processing in the template.
     ping_to_metrics: dict[str, dict[str, list[metrics.Metric]]] = defaultdict(dict)
@@ -129,13 +145,13 @@ def output_rust(
                         + f"{metric.type}:{metric.name}."
                         + " Reach out to Glean team to add support for this"
                         + " metric type."
-                        )
+                    )
                     continue
-                
+
                 for ping in metric.send_in_pings:
                     if metric.type == "event" and metric not in event_metrics:
                         event_metrics.append(metric)
-                    
+
                     metrics_by_type = ping_to_metrics[ping]
                     metrics_list = metrics_by_type.setdefault(metric.type, [])
                     metrics_list.append(metric)
@@ -148,7 +164,7 @@ def output_rust(
     if not ping_to_metrics:
         print(f"❌ No pings with metrics found {PING_METRIC_ERROR_MSG}.")
         return
-    
+
     extension = ".rs"
     filepath = output_dir / (f"server_events{extension}")
     with filepath.open("w", encoding="utf-8") as fd:
@@ -159,4 +175,3 @@ def output_rust(
                 events=event_metrics,
             )
         )
-                    
