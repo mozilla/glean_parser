68634906aa82f48797ad0336498f39cd557f144a
glean-ci <glean-ci@nowhere.com>
2025-04-11 17:09:09 +0000

    [ci skip] updates

--- _sources/installation.md.txt
+++ _sources/installation.md.txt
@@ -37,5 +37,5 @@ $ curl  -OL https://github.com/mozilla/glean_parser/tarball/HEAD
 Once you have a copy of the source, you can install it with:
 
 ```sh
-$ pip install .
+$ uv sync
 ```
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '17.1.1.dev2+g360c9e88',
+    VERSION: '17.1.0.post1.dev6+gd38779c2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
