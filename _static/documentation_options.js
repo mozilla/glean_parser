817d9c39f28c761403ff88ba0f1cd2134ee1a88a
glean-ci <glean-ci@nowhere.com>
2025-06-23 12:08:38 +0000

    [ci skip] updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '17.1.0.post1.dev8+g36a1bdc8',
+    VERSION: '17.2.0.post1.dev1+g023558dc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
