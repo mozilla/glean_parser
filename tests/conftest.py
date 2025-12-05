ab63795968ab4b62dc301b27e6cabf53757a34a9
Jan-Erik Rediger <janerik@fnordig.de>
2025-12-05 13:19:37 +0100

    Always skip Node.js tests on Node.js v24
    `mozlog` uses `intel` which uses the builtin `utils.isError`
    That function was removed in v24
    For now we skip those tests on newer Node.
    
    See https://nodejs.org/api/deprecations.html#DEP0048
    
    The error:
    
        /tmp/pytest-of-circleci/pytest-0/test_logging_events_ping_with_0/node_modules/intel/lib/record.js:56
            if (a && ((isErr = util.isError(a)) || a instanceof Trace)) {
                                    ^
    
        TypeError: util.isError is not a function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 
 
 def pytest_addoption(parser):
@@ -46,6 +47,17 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "node_dependency" in item.keywords:
                 item.add_marker(skip_node)
+    else:
+        # Node.js removed some deprecated items currently in use by dependencies of mozlog.
+        # We can't fix that.
+        node_version = subprocess.check_output(["node", "--version"]).decode("utf-8").strip()
+        major_version = int(node_version[1:].split(".")[0])
+        print(f"jer. {node_version=}, {major_version=}")
+        if major_version >= 24:
+            skip_node = pytest.mark.skip(reason="Node.js < v24 required")
+            for item in items:
+                if "node_dependency" in item.keywords:
+                    item.add_marker(skip_node)
 
     if not config.getoption("--run-ruby-tests"):
         skip_ruby = pytest.mark.skip(reason="Need --run-ruby-tests option to run")
